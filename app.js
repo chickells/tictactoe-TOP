50718a35af8f7cfd448f012198c78dd6553b45ff
Chase <cnickellsumn@gmail.com>
2023-02-01 17:16:20 -0800

    setup skeleton and wrote outline logic in plain english

--- app.js
+++ app.js
@@ -8,8 +8,20 @@ let gameBoard = (() => {
     const player1 = playerInitializer ('player 1', 'X', false, true);
     const player2 = playerInitializer ('player 2', 'O', false, false);
 
+    
+
+    // let winner = null;
+
+    // let turns = 0;
+
+    // let board = [];
+
+    const cells = document.querySelectorAll('.cell');
+    const statusText = document.querySelector('#statusText');
+    const restartBtn = document.querySelector('#restartBtn');
+
     // possible win combos
-    const winCombos = [
+    const winConditions = [
         [0,1,2],
         [0,3,6],
         [3,4,5],
@@ -20,17 +32,50 @@ let gameBoard = (() => {
         [0,4,8]
     ];
 
-    let winner = null;
+    let options = ["", "", "", "", "", "", "", "", ""];
 
-    let turns = 0;
+    let currentPlayer = "X";
+    let running = false;
 
-    let board = [];
+    initializeGame();
 
-    let 
+    function initializeGame() {
+        cells.forEach(cell => addEventListener('click', cellClicked));
+        restartBtn.addEventListener('click', restartGame);
+        statusText.textContent = `${currentPlayer}'s turn:`;
+        running = true;
+    }
+
+    function cellClicked() {
+        // what should happen here?
+        // it should set text content to current player/current player mark
+        // it should change turn
+        // it should check winConditions
+        // it should keep track of total turns and check if tie
+
+        // their solution does this:
+        // set cellIndex to the value of this.cellIndex
+        // then checks if spot has already been clicked/if game isn't running, returns nothing
 
 
+        // then calls updateCell(this, cellIndex), which finds this cell
+        // and updates text content to player mark, then updates
+        // options by assigning the index from this.cellIndex
 
+        // then changes player and updates statusText
+        
+        // then checks winner
+        // does a for loop for i < winConditions.length
+        // assigns array to 
 
+    }
+
+    function restartGame () {
+
+    }    
+
+    console.log('ur mom');
+    
 
 
 
