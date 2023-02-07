478c1e941ad74246a4cfba874f451376c9ede062
Chase <cnickellsumn@gmail.com>
2023-02-07 07:48:57 -0800

    setup skeleton

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 // fuck all this noise, restarting with WebDevSimplified youtube video
 // https://www.youtube.com/watch?v=Y-GkMjUZsmM
 
+const gameBoard = (() => {
+    const cellElements = document.querySelectorAll('[data-cell]')
+    
+    cellElements.forEach(cell => {
+        cell.addEventListener('click', handleClick, { once: true})
+    })
+
+    function handleClick(e) {
+        // place mark
+        // check for win
+        // check for draw
+        // switch turns
+    }
+})()
+
+
