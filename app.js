b04be363c3b6b601c70a6491d42149120e0d6602
Chase <cnickellsumn@gmail.com>
2023-02-08 09:46:24 -0800

    things are working now, this is a horrible commit message lol

--- app.js
+++ app.js
@@ -2,74 +2,106 @@
 // https://www.youtube.com/watch?v=Y-GkMjUZsmM
 
 const gameBoard = (() => {
-    const cellElements = document.querySelectorAll('[data-cell]')
-    
-    cellElements.forEach(cell => {
-        cell.addEventListener('click', handleClick, { once: true})
-    })
-    
     const X_CLASS = 'x'
     const CIRCLE_CLASS = 'circle'
-    let circleTurn
-    let currentTurn
+    const WINNING_COMBOS = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    const cellElements = document.querySelectorAll('[data-cell]')
+    const board = document.getElementById('board')
 
+    // container for bg popup
+    const winningMessageElement = document.getElementById('winningMessage')
+    
+    // actual text
+    const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+
+    let circleTurn
+
+    startGame()
+       
+    function startGame() {
+        circleTurn = false
+        cellElements.forEach(cell => {
+            cell.addEventListener('click', handleClick, { once: true})
+        })
+        setBoardHoverClass()
+    }
+    
     function handleClick(e) {
-        // place mark
-        // this.textContent = currentTurn
+        // place mark        
         const cell = e.target
         const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
-        placeMark(cell, currentClass) 
-        swapTurns(currentTurn)
-        
-        // check for win
-        //check if array matches possible winning outcomes manually
-        // or maybe there's a more efficient way to do this
-
-        // check for draw
-        // if none of the winning sets are met AND if turn count hits 9,
-        // then return DRAW
-
-        // switch turns
-        // keep a rolling variable currentTurn
-
-        // console.log(`Current turn is ${currentTurn}`);
-
-        // if (currentTurn = 'X') {
-        //     currentTurn = 'O'
-        // } else {
-        //     currentTurn = 'X'
-        // }
-
-        // // currentTurn == 'X' ? currentTurn == 'O' : currentTurn == 'X'
-        // console.log(`Now it's ${currentTurn}'s turn`);
-
+        placeMark(cell, currentClass)
+        if (checkWin(currentClass)){
+            console.log('win');
+            endGame(false)
+        } else if (isDraw()) {
+            console.log('draw');
+            endGame(true)
+        } else {
+            console.log('new turn');
+            swapTurns()
+            setBoardHoverClass()
+        }        
     }
 
     function placeMark (cell, currentClass) {
         cell.classList.add(currentClass)
     }
 
-    function swapTurns () {
-        // switch turns
-        // keep a rolling variable currentTurn
+    function swapTurns() {
+        circleTurn = !circleTurn
+      }
 
-        // YT vid declares turn with CSS styling, making shapes
-        // with css classes
-        // so he has swapTurns just toggling Circle Class
-        circleTurn != circleTurn
-    }
+    function setBoardHoverClass() {
+        board.classList.remove(X_CLASS)
+        board.classList.remove(CIRCLE_CLASS)
+        if (circleTurn) {
+          board.classList.add(CIRCLE_CLASS)
+        } else {
+          board.classList.add(X_CLASS)
+        }
+      }
 
-    //     console.log(`Current turn is ${currentTurn}`);
+      // checks if 
+      function checkWin(currentClass) {
+        return WINNING_COMBOS.some(combination => {
+            return combination.every(index => {
+                return cellElements[index].classList.contains(currentClass)
+            })
+        })
+      }
 
-    //     if (currentTurn = 'X') {
-    //         currentTurn = 'O'
-    //     } else {
-    //         currentTurn = 'X'
-    //     } currentTurn
+      // checks if every cell contains either class in the class lists, which is an array
+      // returns BOOL
+      function isDraw() {
+        // the [...____] DESTRUCTURES the cellElements into an array,
+        // which then can call the '.every' function on it, lfg
+        return [...cellElements].every(cell => {
+          return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+        })
+      }
 
-    //     // currentTurn == 'X' ? currentTurn == 'O' : currentTurn == 'X'
-    //     console.log(`Now it's ${currentTurn}'s turn`);
-    // }
+      // endGame only fired if isDraw or checkWin returns true
+      // if Draw, then sets content to draw and shows button
+      // if win, displays winner and shows button
+      function endGame(draw) {
+        if (draw) {
+          winningMessageTextElement.innerText = 'Draw!'
+        } else {
+          winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+        }
+        winningMessageElement.classList.add('show')
+      }
+
 })()
 
 
